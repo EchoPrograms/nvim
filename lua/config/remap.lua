2bec77d5191d9a117aca105a13d0e42bf89c2bb0
EchoPrograms <brogan.oberhaus30@gmail.com>
2026-03-02 13:33:21 -0500

    keybinding overlap fix

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -134,3 +134,31 @@ vim.keymap.set("n", "<leader>ca", "<cmd>CodeCompanionActions<cr>", { noremap = t
 -- Prompt library examples
 vim.keymap.set("n", "<leader>ce", "<cmd>CodeCompanion /explain<cr>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>cf", "<cmd>CodeCompanion /fix<cr>", { noremap = true, silent = true })
+
+
+-- nvim-dap // https://sookocheff.com/post/vim/neovim-java-ide/
+function nnoremap(rhs, lhs, desc)
+  vim.keymap.set("n", rhs, lhs, { desc  = desc })
+end
+
+nnoremap("<leader>bb", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", "Set breakpoint")
+nnoremap("<leader>bc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>", "Set conditional breakpoint")
+nnoremap("<leader>bl", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>", "Set log point")
+nnoremap('<leader>br', "<cmd>lua require'dap'.clear_breakpoints()<cr>", "Clear breakpoints")
+nnoremap('<leader>ba', '<cmd>Telescope dap list_breakpoints<cr>', "List breakpoints")
+
+nnoremap("<leader>rc", "<cmd>lua require'dap'.continue()<cr>", "Continue")
+nnoremap("<leader>rj", "<cmd>lua require'dap'.step_over()<cr>", "Step over")
+nnoremap("<leader>rk", "<cmd>lua require'dap'.step_into()<cr>", "Step into")
+nnoremap("<leader>ro", "<cmd>lua require'dap'.step_out()<cr>", "Step out")
+nnoremap('<leader>rd', "<cmd>lua require'dap'.disconnect()<cr>", "Disconnect")
+nnoremap('<leader>rt', "<cmd>lua require'dap'.terminate()<cr>", "Terminate")
+nnoremap("<leader>rr", "<cmd>lua require'dap'.repl.toggle()<cr>", "Open REPL")
+nnoremap("<leader>rl", "<cmd>lua require'dap'.run_last()<cr>", "Run last")
+nnoremap('<leader>ri', function() require"dap.ui.widgets".hover() end, "Variables")
+nnoremap('<leader>r?', function() local widgets=require"dap.ui.widgets";widgets.centered_float(widgets.scopes) end, "Scopes")
+nnoremap('<leader>rf', '<cmd>Telescope dap frames<cr>', "List frames")
+nnoremap('<leader>rh', '<cmd>Telescope dap commands<cr>', "List commands")
+
+
+
