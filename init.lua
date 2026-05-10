9865da4612a8117eb029d6265eb4517a6cead04f
EchoPrograms <brogan@brogano.dev>
2026-05-09 21:28:18 -0400

    folding

--- init.lua
+++ init.lua
@@ -7,6 +7,63 @@ require("autoclose").setup()
 
 vim.lsp.inlay_hint.enable(true)
 
+-- Folding 
+vim.o.foldcolumn = '1' -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+local handler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, {chunkText, hlGroup})
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    table.insert(newVirtText, {suffix, 'MoreMsg'})
+    return newVirtText
+end
+
+require('ufo').setup({
+    provider_selector = function(bufnr, filetype, buftype)
+        return {'treesitter', 'indent'}
+    end,
+    fold_virt_text_handler = handler
+})
+
+vim.keymap.set('n', '<CR>', function()
+  local winid = vim.api.nvim_get_current_win()
+  local line = vim.api.nvim_win_get_cursor(winid)[1]
+  local foldlevel = vim.fn.foldlevel(line)
+
+  if foldlevel > 0 then
+    vim.cmd('normal! za')
+  else
+    vim.cmd('normal! <CR>')
+  end
+end, { silent = true })
+
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
 -- Appearance
 vim.diagnostic.config({
   -- Use the default configuration
